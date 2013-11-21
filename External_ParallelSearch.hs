d1561113d92f2304642b810fb319b42747f7125d
Bastian Holst <bastianholst@gmx.de>
2013-11-21 14:56:44 +0100

    Switch to IOList for parallel strategies

--- External_ParallelSearch.hs
+++ External_ParallelSearch.hs
@@ -1,5 +1,6 @@
 import Control.Monad.SearchTree
 import MonadSearch
+import MonadList
 import Strategies
 import Control.Parallel.Strategies
 import qualified Curry_Prelude as CP
@@ -12,20 +13,21 @@ external_d_C_getAllValues :: NormalForm a =>
                           -> CP.C_IO (CP.OP_List a)
 external_d_C_getAllValues sy x c s = fromIO $ do
   let tree = encapsulatedSearch x c s
-  hlist <- (getStrategy sy) tree
+  list  <- getStrategy sy tree
+  hlist <- evalIOList list
   return $ hlist2clist hlist
  where
   hlist2clist [] = CP.OP_List
   hlist2clist (x:xs) = CP.OP_Cons x $ hlist2clist xs
 
 data C_Strategy a
-    = Strategy { getStrategy :: SearchTree a -> IO [a] } 
+    = Strategy { getStrategy :: SearchTree a -> IO (IOList a) }
 
 external_d_C_parSearch :: Cover -> ConstStore -> C_Strategy a
-external_d_C_parSearch _ _ = Strategy (return . parSearch)
+external_d_C_parSearch _ _ = Strategy (fromList . parSearch)
 
 external_d_C_evalSearch :: Cover -> ConstStore -> C_Strategy a
-external_d_C_evalSearch _ _ = Strategy (return . evalSearch)
+external_d_C_evalSearch _ _ = Strategy (fromList . evalSearch)
 
 external_d_C_fairSearch :: Cover -> ConstStore -> C_Strategy a
 external_d_C_fairSearch _ _ = Strategy fairSearch
